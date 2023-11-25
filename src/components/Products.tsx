9a90a63ff7b499068301f06b9997e1179309db58
Kirill Zhirnov <ekvixgroup@gmail.com>
2023-11-25 19:37:35 +0100

    minor fixes

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode} from 'react';
 import clsx from 'clsx';
 
-export default function Products({children, className}: IProductsProps) {
+export default function Products({children, className, all, sm, md, lg, xl, xxl}: IProductsProps) {
 	return (
 		<div className={clsx('bdl-products', className)}>
 			{children}
@@ -9,7 +9,18 @@ export default function Products({children, className}: IProductsProps) {
 	);
 }
 
+interface IGapPerRow {
+	gap: 10 | 20 | 30 | 40 | 50,
+	perRow: 1 | 2 | 3 | 4 | 5 | 6
+}
+
 export interface IProductsProps {
 	children: ReactNode | ReactNode[];
 	className?: string;
+	all?: IGapPerRow,
+	sm?: IGapPerRow,
+	md?: IGapPerRow,
+	lg?: IGapPerRow,
+	xl?: IGapPerRow,
+	xxl?: IGapPerRow
 }
